e969dc07703b88918afd1b8c53555caa9cf8c743
Jen Berigan <jenberigan@gmail.com>
2021-07-25 17:09:33 -0500

    adding props to create reusable components

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,28 +1,53 @@
-function Header() {
+function Header(props) {
     return (
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{ props.title }</h1>
+            <span className="stats">Players: { props.totalPlayers }</span>
         </header>
     );
 }
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Jen
+                { props.name }
             </span>
+            <Counter score={ props.score } />
+        </div>
+    );    
+}
 
-            <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <span className="counter-score">35</span>
-                <button className="counter-action increment"> + </button>
-            </div>
+const Counter = (props) => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">{ props.score }</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );   
+}
+
+const App = () => {
+    return(
+        <div className="scoreboard">
+            <Header 
+                title="scoreboard" 
+                totalPlayers={1} 
+
+            />
+
+            {/* Players List */}
+            <Player name="Jen" score={50} />
+            <Player name="Ashley" score={30} />
+            <Player name="George" score={80} />
+            <Player name="Jill" score={45} />
         </div>
     );
 }
+
+
 ReactDOM.render (
-    <Player />,
+    <App />,
     document.getElementById('root')
 );
